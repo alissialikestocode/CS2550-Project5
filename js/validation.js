3deba2f0badfad6dd21d575159c2a95a1e916f1f
alissia <alissia.d.austell@gmail.com>
2025-04-18 16:58:42 -0600

    fixing

--- js/validation.js
+++ js/validation.js
@@ -156,7 +156,7 @@ function setElementValidity(id, valid, message) {
 
     if (valid) {                        // It has a value
         el.setCustomValidity('');       // Sets to no error msg and field is valid
-        errorDiv.innerHTML = "";
+        // errorDiv.innerHTML = "";
     }
     else {
         el.setCustomValidity(message);  // Sets error msg and field gets invalid state
