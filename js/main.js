d9d44355df6a91b2ae03bf1ae9e9788b145b50c3
alissia <alissia.d.austell@gmail.com>
2025-04-18 14:28:46 -0600

    set up

---
+++ js/main.js
@@ -0,0 +1,104 @@
+/*
+    Changes made:
+        --I didn't have any JavaScript problems to change from Project 4
+        --Added this change list to the top of index.js
+        --Adjusted JavaScript for switching between tabs to include the filename
+            change (index.js to main.js) and new Forms page
+        --Organized JavaScript files in a subfile
+*/
+
+// Switch between tabs on page
+function openTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tabs");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+
+    if (tabName == 'home-container') {
+        document.getElementById('home-container').style.display = 'grid';
+    }
+}
+
+// Event handler for theme change button click
+document.getElementById("themeChange").addEventListener("click", function() {
+
+    toggleStylesheet("theme.css");
+    //insert 'link' element into head just below styles.css reference
+
+}, { passive: true });
+
+function toggleStylesheet(href, onoff) {
+    var existingNode = 0 //get existing stylesheet node if it already exists:
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        if (document.styleSheets[i].href && document.styleSheets[i].href.indexOf(href) > -1) existingNode = document.styleSheets[i].ownerNode
+    }
+    if (onoff == undefined) onoff = !existingNode //toggle on or off if undefined
+    if (onoff) { //TURN ON:
+        if (existingNode) return onoff //already exists so cancel now
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'css/theme.css';
+        document.getElementsByTagName('head')[0].appendChild(link);
+
+        // Switch image in header
+        // div.style.backgroundImage = `url('images/Doom-At-Your-Service-001.jpeg')`;
+
+        var imgLogo = document.getElementById("logo-img");
+        imgLogo.src = `images/couple.jpg`;
+        imgLogo.alt = `A picture of Seo In-Guk and Park Bo-Young`;
+
+        var h1 = document.getElementsByTagName("h1")[0];
+        h1.innerText = `Okay Drama Queen! : The DAYS Takeover Edition`;
+
+        // Change Actor photo
+        var img = document.getElementById("actor-photo");
+        img.src = `images/seo_in_guk.jpeg`;
+        img.alt = `A picture of Seo In-Guk`;
+
+        var imgTxt = document.getElementById("actor-name");
+        imgTxt.innerText = `Seo In-Guk`;
+
+        // Change video playing
+        var h3 = document.getElementById("video-container").children[0];
+        h3.innerText = `Watch a trailer for Doom At Your Service`;
+
+        var player = document.getElementById("video-container").children[1];
+        player.src = `https://www.youtube.com/embed/SMSVgUpQVps?si=8OfkzfG0ag7yj9yR`;
+            
+     } else { //TURN OFF:
+        if (existingNode) existingNode.parentNode.removeChild(existingNode)
+        
+        // Change logo and header back
+        var imgLogo = document.getElementById("logo-img");
+        imgLogo.src = `images/crown_orig.png`;
+        imgLogo.alt = `A black crown with three peaks`;
+
+        var h1 = document.getElementsByTagName("h1")[0];
+        h1.innerText = `Okay Drama Queen!`;
+        
+        // Change actor photo back
+        var img = document.getElementById("actor-photo");
+        img.src = `images/nam_joo_hyuk3.jpg`;
+        img.alt = `A picture of Nam Joo-Hyuk`;
+
+        var imgTxt = document.getElementById("actor-name");
+        imgTxt.innerText = `Nam Joo-Hyuk`;
+        
+        // Change video playing back
+        var h3 = document.getElementById("video-container").children[0];
+        h3.innerText = `Watch Oh My Ghost Episode 1`;
+
+        var player = document.getElementById("video-container").children[1];
+        player.src = `https://www.youtube.com/embed/HwbwE5LK3lA?si=5miug9Mvs2_EZXwW`;
+    }
+    return onoff;
+}
+
+
+// Entry point for js validation, happens after everything has been loaded/rendered on page
+document.addEventListener("DOMContentLoaded", function(event) {
+    initValidation("myform");
+});
---
+++ js/validation.js
@@ -0,0 +1,163 @@
+/*   
+ 
+Project:  Project 5
+Name: Alissia Austell Huntzinger
+Submitted: 4/18/25
+ 
+I declare that the following source code was written by me, or provided
+by the instructor for this project. I understand that copying source
+code from any other source, providing source code to another student, 
+or leaving my code on a public web site constitutes cheating.
+I acknowledge that  If I am found in violation of this policy this may result
+in a zero grade, a permanent record on file and possibly immediate failure of the class.
+ 
+Reflection (1-2 paragraphs):  I really enjoyed writing this program, but had trouble with blah blah blah.  
+ 
+*/
+
+// Establish global variables for validating
+let phoneRegex = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;        // regex sourced from https://uibakery.io/regex-library/phone-number
+let emailRegex = /[\w]*@[\w]*.{1}(com|gov|edu|io|net){1}/;
+let zipCodeRegex = /(?<zip1>\d{5})([-]?(?<zip2>\d{4}))?(?<ERROR>.+)?/
+const stateAbbreviations = [
+    'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA',
+    'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA',
+    'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+    'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+    'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'
+];
+let form = null;
+let successMsg = null;
+
+function initValidation(formID, successId) { 
+    form = document.getElementById(formId);
+    successMsg = document.getElementById(successId);
+
+    let inputs = document.querySelectorAll("input");       
+    for (let input of inputs) {                                 // Add event listeners checking for changes in the form inputs
+        input.addEventListener("change", inputChanged);
+    }
+    form.addEventListener("submit", submitForm);
+}
+
+function inputChanged(ev) {             // When the input is changed add "was-validated" class to element whose event listener triggered the event
+    let el = ev.currentTarget;
+    validateForm();
+    
+    // Use .was-validated to show error indications only for single field rather than whole form at once
+    el.classList.add("was-validated");
+}
+
+function submitForm(ev) {
+    console.log("in submit");
+    let form = ev.currentTarget;
+
+    ev.preventDefault();            // Use these to stop the form from submitting
+    ev.stopPropagation();
+
+    validateForm();
+
+    if (!form.checkValidity()) {            //form.checkValidity returns F if any input is invalid, this runs if any input is invalid
+        let inputs = document.querySelectorAll("input");
+        for (let input of inputs) {
+            input.classList.add("was-validated");
+        }
+    }
+    else {
+        form.style.display = "none";
+    }
+}
+
+function validateForm() { 
+    checkRequired("first-name", "First Name is Required");
+    checkRequired("last-name", "Last Name is Required");
+    checkRequired("address", "Address is Required");
+    checkRequired("city", "City is Required");
+
+    if (checkRequired("state", "State is Required")) {
+        validateState("state", "Not a valid State, enter two digit code e.g., UT");
+    }
+
+    if (checkRequired("email", "Email Address is required")) {
+        checkFormat("email", "email format is bad", emailRegex)
+    }
+
+    if (checkRequired("zip", "Zip Code is Required")) {
+        checkFormat("zip", `malformed zip-code, please use either "#####", or "#####-#### format.`, zipCodeRegex)
+    }
+
+    if (checkRequired("phone", "Phone is required")) {
+        checkFormat("phone", "phone format is bad", phoneRegex)
+    }
+
+    checkRequired("newspaper", "you must select at least one!");
+}
+
+function validateState(id, msg) {
+    let el = document.getElementById(id);
+    let valid = false;
+
+    let elInput = el.value;                 // Returns a string
+    elInput = elInput.toUpperCase();        // Conver string to uppercase
+
+    if (stateAbbreviations.includes(elInput)) {  // Looks for elInput in array, ret.s T if in array
+        valid = true;
+    }
+
+    setElementValidity(id, valid, msg);     // shows red or green borders, error msgs (inserts them in erroMsg div)
+}
+
+function checkFormat(id, msg, regex) { 
+    let valid = false;
+    let elemVal = getElementById(id).value;
+    
+    const result = regex.test(elemVal);
+    if (!result) { 
+        valid = true;
+    }
+
+    setElementValidity(id, valid, msg);
+    return valid
+}
+
+function checkRequired(id, message) { 
+    let el = document.getElementById(id);
+    let valid = false;
+    let type = el.type;
+    switch (type) {
+        case 'text':
+        case 'password':
+            if (el.value.trim() != "") {
+                valid = true;
+            }
+            break;
+        
+        case 'checkbox':
+            let allCheckboxes = documquerySelectorAll("input[name=find-page]");
+            
+            for (let box of allCheckboxes) {
+                if (box.checked) {
+                    valid = true;
+                }
+            }
+            break;
+    }
+    setElementValidity(id, valid, message);
+
+    return valid;
+}
+
+// TODO
+function setElementValidity(id, valid, message) { 
+    let el = document.getElementById(id);
+
+    if (valid) {                        // It has a value
+        el.setCustomValidity('');       // Sets to no error msg and field is valid
+    }
+    else {
+        el.setCustomValidity(message);  // Sets error msg and field gets invalid state
+
+        let errorDiv = document.getElementById("errorMsg");
+        errorDiv.innerHTML = "<p>" + message + "</p>";
+    }
+}
